293f5d244878392f4375303d4b5bd6c88e4737fe
elsasinaga <elsamonikasinaga@gmail.com>
2024-09-15 21:44:48 +0700

    Upload folder tugas 4

---
+++ src/MainProgram.java
@@ -0,0 +1,14 @@
+import com.polban.jtk.sales.Product;
+import com.polban.jtk.sales.Sales;
+
+public class MainProgram {
+    public static void main(String []args){
+        // membuat objek produk
+        Product product = new Product("Liptint", 45000, 10);
+        Sales Penjualan = new Sales (product);
+
+        Penjualan.sellProduct(5);
+        Penjualan.restockProduct(1);
+        Penjualan.updateProductPrice(12000000);
+    }
+}
